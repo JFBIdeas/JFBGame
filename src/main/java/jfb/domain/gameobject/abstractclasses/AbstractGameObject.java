897b57711cb289ffced1c85747688c58f7dd2c32
IKS-cod <ik.smirnov@yandex.ru>
2024-09-16 21:06:11 +0300

    Исправил абстрактный класс AbstractGameObject, удалил лишние поля

--- src/main/java/jfb/domain/gameobject/abstractclasses/AbstractGameObject.java
+++ src/main/java/jfb/domain/gameobject/abstractclasses/AbstractGameObject.java
@@ -6,21 +6,12 @@ import java.awt.*;
 
 public abstract class AbstractGameObject implements GameObject {
 
-    //параметр двигается объект или нет
-    boolean moveGameObject;
-    //количество здоровья;
-    private int liveGameObject;
-    //скорость объекта
-    private int speedGameObject;
-    //кол-во созданных объектов;
-    public static int countGameObject;
     //координаты x, y;
     private int x, y;
     //параметры ширина и высота
     private int width, height;
 
     public AbstractGameObject(int x, int y, int width, int height) {
-        countGameObject++;
         this.x = x;
         this.y = y;
         this.width = width;
@@ -39,7 +30,6 @@ public abstract class AbstractGameObject implements GameObject {
 
     @Override
     public void setPosition(int x, int y) {
-
     }
 
     @Override
@@ -54,11 +44,9 @@ public abstract class AbstractGameObject implements GameObject {
 
     @Override
     public void setSize(int width, int height) {
-
     }
 
     @Override
     public void render(Graphics g) {
-
     }
 }
