e97b2c199230b3be6e2fecb86dff5208e17ebfa8
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-09-16 09:30:39 +0500

    ActionGameObject and StaticGameObject classes added

---
+++ src/main/java/jfb/domain/gameobject/abstractclasses/ActionGameObject.java
@@ -0,0 +1,7 @@
+package jfb.domain.gameobject.abstractclasses;
+
+public class ActionGameObject extends GameObject{
+    public ActionGameObject() {
+        setSize(16,16);
+    }
+}
---
+++ src/main/java/jfb/domain/gameobject/abstractclasses/StaticGameObject.java
@@ -0,0 +1,10 @@
+package jfb.domain.gameobject.abstractclasses;
+
+public class StaticGameObject extends GameObject{
+
+    public StaticGameObject() {
+        super();
+        setSize(16,16);
+    }
+
+}
