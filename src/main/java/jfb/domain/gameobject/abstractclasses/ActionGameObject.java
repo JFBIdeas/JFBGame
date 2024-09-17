fc151d8a619567cbe95778987b672a761d624dca
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-09-17 08:20:58 +0500

    ActionGameObject and StaticGameObject classes was changed

--- src/main/java/jfb/domain/gameobject/abstractclasses/ActionGameObject.java
+++ src/main/java/jfb/domain/gameobject/abstractclasses/ActionGameObject.java
@@ -1,7 +1,10 @@
 package jfb.domain.gameobject.abstractclasses;
 
-public class ActionGameObject extends GameObject{
-    public ActionGameObject() {
-        setSize(16,16);
+
+public class ActionGameObject extends AbstractGameObject {
+
+    public ActionGameObject(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        setPosition(16,16);
     }
 }
--- src/main/java/jfb/domain/gameobject/abstractclasses/StaticGameObject.java
+++ src/main/java/jfb/domain/gameobject/abstractclasses/StaticGameObject.java
@@ -1,10 +1,9 @@
 package jfb.domain.gameobject.abstractclasses;
 
-public class StaticGameObject extends GameObject{
+public class StaticGameObject extends AbstractGameObject{
 
-    public StaticGameObject() {
-        super();
-        setSize(16,16);
+    public StaticGameObject(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        setPosition(16,16);
     }
-
 }
