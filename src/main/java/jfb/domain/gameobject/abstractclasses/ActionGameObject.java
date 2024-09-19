e216e3c2114ad0c5ed7cfaabefe9c033b737fafe
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-09-19 08:15:31 +0500

    Implementations added in ActionGameObject and StaticGameObject classes

--- src/main/java/jfb/domain/gameobject/abstractclasses/ActionGameObject.java
+++ src/main/java/jfb/domain/gameobject/abstractclasses/ActionGameObject.java
@@ -1,10 +1,60 @@
 package jfb.domain.gameobject.abstractclasses;
 
 
-public class ActionGameObject extends AbstractGameObject {
+import jfb.domain.gameobject.interfaces.Animatable;
+import jfb.domain.gameobject.interfaces.Damageable;
+import jfb.domain.gameobject.interfaces.Movable;
 
-    public ActionGameObject(int x, int y, int width, int height) {
+public class ActionGameObject extends AbstractGameObject implements Animatable, Damageable, Movable {
+    private int health;
+    private int speedX;
+    private int speedY;
+
+    public ActionGameObject(int x, int y, int width, int height, int health, int speedX, int speedY) {
         super(x, y, width, height);
-        setPosition(16,16);
+        this.health = health;
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+    @Override
+    public int getHealth() {
+        return health;
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        health -= damage;
+    }
+
+    @Override
+    public void heal(int amount) {
+        health += amount;
+    }
+
+    @Override
+    public void move() {
+        setPosition(getX()+speedX, getY()+speedY);
+    }
+
+    @Override
+    public void setSpeed(int speedX, int speedY) {
+        this.speedX = speedX;
+        this.speedY = speedY;
+    }
+
+    @Override
+    public void startAnimation() {
+
+    }
+
+    @Override
+    public void stopAnimation() {
+
+    }
+
+    @Override
+    public void updateAnimation() {
+
     }
 }
--- src/main/java/jfb/domain/gameobject/abstractclasses/StaticGameObject.java
+++ src/main/java/jfb/domain/gameobject/abstractclasses/StaticGameObject.java
@@ -1,9 +1,32 @@
 package jfb.domain.gameobject.abstractclasses;
 
-public class StaticGameObject extends AbstractGameObject{
+import jfb.domain.entity.player.Player;
+import jfb.domain.gameobject.interfaces.Animatable;
+import jfb.domain.gameobject.interfaces.Interactable;
+
+public class StaticGameObject extends AbstractGameObject implements Animatable, Interactable {
 
     public StaticGameObject(int x, int y, int width, int height) {
         super(x, y, width, height);
-        setPosition(16,16);
+    }
+
+    @Override
+    public void interact(Player player) {
+
+    }
+
+    @Override
+    public void startAnimation() {
+
+    }
+
+    @Override
+    public void stopAnimation() {
+
+    }
+
+    @Override
+    public void updateAnimation() {
+
     }
 }
