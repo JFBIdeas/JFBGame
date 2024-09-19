45239a22cfe704896899b8bb54b5d40721cbec99
Sagid Kadirov <sagidkadirov1999@gmail.com>
2024-09-19 19:45:13 +0500

    ActionGameObject and StaticGameObject was changed

--- src/main/java/jfb/domain/gameobject/abstractclasses/ActionGameObject.java
+++ src/main/java/jfb/domain/gameobject/abstractclasses/ActionGameObject.java
@@ -2,39 +2,22 @@ package jfb.domain.gameobject.abstractclasses;
 
 
 import jfb.domain.gameobject.interfaces.Animatable;
-import jfb.domain.gameobject.interfaces.Damageable;
 import jfb.domain.gameobject.interfaces.Movable;
 
-public class ActionGameObject extends AbstractGameObject implements Animatable, Damageable, Movable {
-    private int health;
+public class ActionGameObject extends AbstractGameObject implements Animatable, Movable {
+
     private int speedX;
     private int speedY;
 
-    public ActionGameObject(int x, int y, int width, int height, int health, int speedX, int speedY) {
+    public ActionGameObject(int x, int y, int width, int height, int speedX, int speedY) {
         super(x, y, width, height);
-        this.health = health;
         this.speedX = speedX;
         this.speedY = speedY;
     }
 
     @Override
-    public int getHealth() {
-        return health;
-    }
-
-    @Override
-    public void takeDamage(int damage) {
-        health -= damage;
-    }
-
-    @Override
-    public void heal(int amount) {
-        health += amount;
-    }
-
-    @Override
     public void move() {
-        setPosition(getX()+speedX, getY()+speedY);
+        setPosition(getX() + speedX, getY() + speedY);
     }
 
     @Override
--- src/main/java/jfb/domain/gameobject/abstractclasses/StaticGameObject.java
+++ src/main/java/jfb/domain/gameobject/abstractclasses/StaticGameObject.java
@@ -1,18 +1,11 @@
 package jfb.domain.gameobject.abstractclasses;
 
-import jfb.domain.entity.player.Player;
 import jfb.domain.gameobject.interfaces.Animatable;
-import jfb.domain.gameobject.interfaces.Interactable;
 
-public class StaticGameObject extends AbstractGameObject implements Animatable, Interactable {
+public class StaticGameObject extends AbstractGameObject implements Animatable {
 
     public StaticGameObject(int x, int y, int width, int height) {
         super(x, y, width, height);
-    }
-
-    @Override
-    public void interact(Player player) {
-
     }
 
     @Override
