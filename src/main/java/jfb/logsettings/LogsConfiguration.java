3724f2065a8dfb2c6be7cc94b060ea2100efddb1
Prophetir <123047884+Prophetir@users.noreply.github.com>
2024-09-21 19:51:10 +0700

    Исправление создания файла и директории. Добавлена создание файла и директории в src/log по относительному пути.

--- src/main/java/jfb/logsettings/LogsConfiguration.java
+++ src/main/java/jfb/logsettings/LogsConfiguration.java
@@ -14,6 +14,7 @@ public class LogsConfiguration {
     private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss, yyyy.MM.dd");
     private static final Path PATH_TO_DIRECTORY_LOG = Path.of("src/log");
     private static final Path PATH_TO_LOG_FILE = Path.of(PATH_TO_DIRECTORY_LOG + "/log.txt");
+    private static int stringCount;
 
     public static LogsConfiguration getInstance() {
         synchronized (LogsConfiguration.class) {
@@ -22,6 +23,32 @@ public class LogsConfiguration {
     }
 
     public static void writeLog(String message) {
+        checkFile();
+
+        try (BufferedWriter fileWriter = Files.newBufferedWriter(PATH_TO_LOG_FILE, StandardOpenOption.APPEND)) {
+            deleteLog();
+
+            fileWriter.write(String.format("%d - {%s} %s\n", stringCount, TIME_FORMATTER.format(LocalDateTime.now()), message));
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static void deleteLog() {
+        try {
+            stringCount = Files.readAllLines(PATH_TO_LOG_FILE).size() + 1;
+
+            if (stringCount > 15000) {
+                stringCount = 1;
+
+                Files.newBufferedWriter((PATH_TO_LOG_FILE), StandardOpenOption.TRUNCATE_EXISTING);
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private static void checkFile() {
         try {
             if (!Files.exists(PATH_TO_DIRECTORY_LOG)) {
                 Files.createDirectories(PATH_TO_DIRECTORY_LOG);
@@ -30,30 +57,18 @@ public class LogsConfiguration {
             if (!Files.exists(PATH_TO_LOG_FILE)) {
                 Files.createFile(PATH_TO_LOG_FILE);
             }
-
-            try (BufferedWriter fileWriter = Files.newBufferedWriter(PATH_TO_LOG_FILE, StandardOpenOption.APPEND)) {
-
-                int stringCount = Files.readAllLines(PATH_TO_LOG_FILE).size() + 1;
-
-                if (stringCount > 15000) {
-                    stringCount = 1;
-                    deleteLog();
-                }
-
-                fileWriter.write(stringCount + " - " + "{" + TIME_FORMATTER.format(LocalDateTime.now()) + "} " + message + "\n");
-            }
-        } catch (IOException exception) { exception.printStackTrace(); }
-    }
-
-    private static void deleteLog() {
-        try {
-            Files.newBufferedWriter((PATH_TO_LOG_FILE), StandardOpenOption.TRUNCATE_EXISTING);
-        } catch (IOException e) { e.printStackTrace(); }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public static void writeExceptionStackTraceInLogFile(StackTraceElement[] stackTraceElements) {
         writeLog("{" + TIME_FORMATTER.format(LocalDateTime.now()) + "} "
                     + Arrays.stream(stackTraceElements).map(StackTraceElement::toString).toList()
                     + "\n");
+
+        writeLog(String.format("{%s} %s\n",
+                TIME_FORMATTER.format(LocalDateTime.now()),
+                Arrays.stream(stackTraceElements).map(StackTraceElement::toString).toList()));
     }
 }
