734f47dbb6a48b132c239252b6934f57dd2a7542
IKS-cod <ik.smirnov@yandex.ru>
2024-09-21 11:05:58 +0300

    У блал из main лишнее

--- src/main/java/jfb/Main.java
+++ src/main/java/jfb/Main.java
@@ -8,7 +8,6 @@ import java.util.Properties;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println(ConfigLoader.loadConfig("application.properties"));
 
     }
 }
