27bae57429bc115ae7e1b4b3d84abf06b3ce450d
Denis <Oleborn@mail.ru>
2024-10-02 10:22:32 +0300

    fix

--- src/main/java/jfb/view/Window.java
+++ src/main/java/jfb/view/Window.java
@@ -5,16 +5,6 @@ import java.awt.*;
 
 public class Window extends JFrame {
     public Window() {
-        super("JFB Game");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        PaintPanel paintPanel = new PaintPanel();
-        setContentPane(paintPanel);
-
-
-        setResizable(false);
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
     }
 
     public void update(){
